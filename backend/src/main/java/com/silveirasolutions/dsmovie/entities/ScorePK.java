0326b180b6ec167c0b2971c8f59705fd41ca849b
Thiago_Ifood <silveirat@grupodimed.com.br>
2022-03-11 01:23:55 -0300

    Inclui o mapeamento dos modelos e carga de dados iniciais da aplicação em banco H2

---
+++ backend/src/main/java/com/silveirasolutions/dsmovie/entities/ScorePK.java
@@ -0,0 +1,23 @@
+package com.silveirasolutions.dsmovie.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@Data
+@Embeddable
+public class ScorePK implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
