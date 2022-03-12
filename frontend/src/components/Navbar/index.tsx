040202c1a27c2eb19f4b2a463c50387d2ae00ab0
Thiago_Ifood <silveirat@grupodimed.com.br>
2022-03-12 18:02:37 -0300

    Alteração link do github mas com muita gratitão a equipe DevSuperior

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar(){
                 <a href="https://github.com/ThiagoKakoSilveira">
                     <div className="dsmovie-contact-container">
                         <GithubIcon/>
-                        <p className="dsmovie-contact-link">/devsuperior</p>
+                        <p className="dsmovie-contact-link">/devThiagoMS</p>
                     </div>
                 </a>
                 </div>
